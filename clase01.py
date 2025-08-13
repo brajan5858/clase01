6bf92bd3d7d96c6dafab9e0d8421537c452a2a38
brajan5858 <brajan.city.3000@gmail.com>
2025-08-12 20:46:07 -0400

    Add files via upload

---
+++ clase01.py
@@ -0,0 +1,29 @@
+print("hola mundo de bierda")
+#variables
+edad = 25 #entero
+altura = 1.75 #flotante
+bandera = True #booleano
+nombre = "desconocido "  #string
+print("print tradicional")
+print("-----------------")
+print("mi nombre es :",nombre)
+print("mi edad es :",edad)
+print("mi altura es :",altura)
+print("estoy vivo :", bandera)
+
+
+print("\nmoderno")
+
+
+print(" \nEntrada de datos")
+print("-------------")
+nombre = input("ingresa tu nonbre:")
+edad = int(input("ingresa tu edad:"))
+altura = float(input("ingresa tu altura:"))
+bandera = input("estas vivo?  (true/false):")
+
+print(f"mi nombre es {nombre}") #cadenas 
+print(f"mi eda es: {edad}"  )
+print(f"mi altura es:{altura}")
+print(f"estoy vivo {bandera}")
+
